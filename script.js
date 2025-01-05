1e52d1c48bf52a5461669aa7ad57677fd43ff464
rb <Yamiizinmake@gmail.com>
2025-01-05 14:27:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const menuLateral = document.getElementById("menu-lateral");
+
+function toggleMenu() {
+    menuLateral.classList.toggle("active");
+}
+
+// Partículas animadas no fundo
+const canvas = document.getElementById("background-animation");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
+class Particle {
+    constructor(x, y, size, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    update() {
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+
+        if (this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height) {
+            this.x = Math.random() * canvas.width;
+            this.y = Math.random() * canvas.height;
+        }
+    }
+}
+
+function initParticles() {
+    particles = [];
+    for (let i = 0; i < 100; i++) {
+        const size = Math.random() * 3 + 1;
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        const velocity = {
+            x: Math.random() * 2 - 1,
+            y: Math.random() * 2 - 1,
+        };
+        const color = "rgba(100, 255, 218, 0.7)";
+        particles.push(new Particle(x, y, size, color, velocity));
+    }
+}
+
+function animateParticles() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particles.forEach((particle) => {
+        particle.update();
+        particle.draw();
+    });
+    requestAnimationFrame(animateParticles);
+}
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    initParticles();
+});
+
+initParticles();
+animateParticles();
